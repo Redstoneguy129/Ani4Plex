c64ca24c5ffa2e5ac8a007ce4a3eb26a95520274
Cameron Whyte <public@cameronwhyte.me>
2021-11-30 15:02:17 +0000

    add frontend

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
 require('dotenv').config();
 
 import express from "express";
+import path from "path";
 
 const app = express();
-const port = 3000;
+const port = 3050;
+console.log(__dirname)
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 import api from "./api/index";
 app.use('/api', api);
 
-app.get('/', (req, res) => {
-    res.send("react app")
-})
-
-import PlexModule from "./modules/PlexModule";
+/*import PlexModule from "./modules/PlexModule";
 const plex = new PlexModule(process.env.AP_SERVER_NAME as string, process.env.AP_LIBRARY_NAME as string, {
     username: process.env.AP_USERNAME as string,
     password: process.env.AP_PASSWORD as string
-});
+});*/
 setTimeout(main, 2000)
+
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 
 async function main() {
-    plex.getShows().then(shows => console.log(shows))
+    //plex.getShows().then(shows => console.log(shows))
 }
