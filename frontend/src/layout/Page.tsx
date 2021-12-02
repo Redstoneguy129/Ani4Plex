fa4606161fd0bf4abf0aee738ee7c9c891859847
Cameron Whyte <public@cameronwhyte.me>
2021-12-02 12:58:21 +0000

    Added multiple progress bars

--- frontend/src/layout/Page.tsx
+++ frontend/src/layout/Page.tsx
@@ -1,8 +1,47 @@
 import React from "react";
 import Show from "../components/Show";
 
+type show = {
+    title: string,
+    watched?: number,
+    episodes: number
+}
+
+const shows: show[] = [
+    {
+        title: "Boku No Hero Academia",
+        watched: 6,
+        episodes: 12
+    },
+    {
+        title: "Boku No Hero Academia",
+        episodes: 4
+    },
+    {
+        title: "Boku No Hero Academia",
+        watched: 24,
+        episodes: 24
+    },
+    {
+        title: "Boku No Hero Academia",
+        watched: 7,
+        episodes: 8
+    }
+];
+
 export default class Page extends React.Component {
+    getShows() {
+        const showsComp: JSX.Element[] = [];
+        shows.forEach(showe => {
+            showsComp.push(<Show title={showe.title} watched={showe.watched} episodes={showe.episodes}/>)
+        });
+        return showsComp;
+    }
     render(): React.ReactNode {
-        return <div><Show></Show></div>
+        return (
+            <div style={{ display: 'flex', marginTop: 15, flexWrap: 'wrap', rowGap: 15, columnGap: 15, alignItems: 'center', justifyContent: 'center'  }}>
+                {this.getShows()}
+            </div>
+        );
     }
 }
