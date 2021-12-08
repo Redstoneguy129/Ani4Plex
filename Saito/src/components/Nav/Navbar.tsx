8ec19f3beb3a4e3f771e2257c9b1c2a19ef5186d
Cameron Whyte <public@cameronwhyte.me>
2021-12-08 22:40:28 +0000

    barely anything today

--- Saito/src/components/Nav/Navbar.tsx
+++ Saito/src/components/Nav/Navbar.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, ReactNode, ReactElement, Children } from "react";
 import NavLogo from "./NavLogo";
 import NavLink from "./NavLink";
 
-const ChildClassNames = ["NavLogo", "NavLink"];
+let ChildClassNames = new Map<string, FunctionComponent<any>>([['NavLogo', NavLogo], ['NavLink', NavLink]]);
 
 type ChildClassName = {
     type: {
@@ -13,18 +13,15 @@ type ChildClassName = {
 function loopKids(children: ReactNode): ReactNode {
     var callback;
     Children.forEach(children, (child: ChildClassName | any) => {
-        console.log(child.type.name);
-        if(ChildClassNames.includes(child.type.name))  {
-
-        }
+        //console.log(child.type.name);
+        console.log(ChildClassNames.get(child.type.name));
     })
     return (<div>test</div>)
 }
 
 const Navbar: FunctionComponent<{ children: ReactNode }> = ({ children }) => (
     <div className="mx-6 my-4">
-        <nav className="flex flex-row w-full rounded-r-full rounded-b-full py-2 bg-gradient-to-br from-yellow-600 to-yellow-400">
-
+        <nav className="flex flex-row w-full rounded-r-full rounded-b-full py-2 bg-blue-500">
             {loopKids(children)}
         </nav>
     </div>
